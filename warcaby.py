d72627b361bbc7cbbf9f23c16ac2d4bdbbad8386
szopa <szopa@amorki.pl>
2013-04-26 01:12:12 +0300

    Update warcaby.py

--- warcaby.py
+++ warcaby.py
@@ -1,13 +1,51 @@
+#class GameBoard(object):
+
+
+
+#     plansza = [1, 2, 1, 2, 1, 2, 1, 2],
+# [2, 1, 2, 1, 2, 1, 2, 1],
+# [1, 2, 1, 2, 1, 2, 1, 2],
+# [2, 1, 2, 1, 2, 1, 2, 1],
+# [1, 2, 1, 2, 1, 2, 1, 2],
+# [2, 1, 2, 1, 2, 1, 2, 1],
+# [1, 2, 1, 2, 1, 2, 1, 2],
+# [2, 1, 2, 1, 2, 1, 2, 1]
+
 
 import pygame
+class pionek(object):
+    def __init__(self, color, col, row ):
+        
+        self.color = kolor
+        self.row = row
+        self.col = col
+
+
+    def ruch(self, kierunek):
+        if kierunek:
+            self.col += 1
+        else:
+            self.col -= 1
+        self.row+=1
+        (x, y) = self.posn
+        new_y_pos = y + self.y_velocity
+        (target_x, target_y) = self.target_posn   # Unpack the position
+        dist_to_go = target_y - new_y_pos         # How far to our floor?
+
+        if dist_to_go < 0:                        # Are we under floor?
+            self.y_velocity = -0.65 * self.y_velocity     # Bounce
+            new_y_pos = target_y + dist_to_go     # Move back above floor
+
+        self.posn = (x, new_y_pos)   
+    
 
 def draw_board(the_board):
     """ Draw a chess board with queens, as determined by the the_board. """
 
     pygame.init()
-    colors = [(255,0,0), (0,0,0)]    # Set up colors [red, black]
+    colors = [(255,255,255), (0,0,0)]    # Set up colors [red, black]
 
-    n = len(the_board)         # This is an NxN chess board.
+    n = 8         # This is an NxN chess board.
     surface_sz = 480           # Proposed physical surface size.
     sq_sz = surface_sz // n    # sq_sz is length of a square.
     surface_sz = n * sq_sz     # Adjust to exactly fit n squares.
@@ -39,10 +77,13 @@ def draw_board(the_board):
                 c_indx = (c_indx + 1) % 2
 
         # Now that squares are drawn, draw the queens.
-        for (col, row) in enumerate(the_board):
-          surface.blit(ball,
-                   (col*sq_sz+ball_offset,row*sq_sz+ball_offset))
+        for row in range(3):
+            for col in range(4):
+                surface.blit(ball, ((2*col+row%2)*sq_sz+ball_offset,row*sq_sz+ball_offset))
 
+        for row in range(5,9):
+            for col in range(4):
+                surface.blit(ball, ((2*col+row%2)*sq_sz+ball_offset,row*sq_sz+ball_offset))
         pygame.display.flip()
 
 
@@ -53,3 +94,4 @@ if __name__ == "__main__":
     # draw_board([6, 4, 2, 0, 5, 7, 1, 3])
     # draw_board([9, 6, 0, 3, 10, 7, 2, 4, 12, 8, 11, 5, 1])  # 13 x 13
     # draw_board([11, 4, 8, 12, 2, 7, 3, 15, 0, 14, 10, 6, 13, 1, 5, 9])
+
